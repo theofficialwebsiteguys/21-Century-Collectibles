97ccf3546265ee5416f86b6c6349686e6fa84186
Full Name <theofficialwebsiteguys@gmail.com>
2024-10-24 11:05:31 -0400

    init build

--- src/app/testimonials/testimonials.component.ts
+++ src/app/testimonials/testimonials.component.ts
@@ -16,40 +16,40 @@ declare var $: any;
 
 export class TestimonialsComponent {
   users = [{
-      name: 'Customer 1',
-      description: 'review',
-      image: 'assets/default.jpg',
+      name: 'Marta K.',
+      description: 'John was a great help to me when I needed to get rid of some collectibles. He purchased my in-laws entire record collection and offered twice as much as the previous dealer I called. Very honest and professional. I highly recommend him.',
+      image: 'assets/reviews/marta-k.jpg',
       rating: 5 // Add rating property
     },
   {
-    name: 'Customer 2',
-    description: 'review',
-    image: 'assets/default.jpg',
+    name: 'Stephanie M.',
+    description: 'Great guy great stuff. I\'m a repeat buyer when in the area!!! Very fair prices and huge selection',
+    image: 'assets/reviews/stephanie-m.jpg',
     rating: 5 // Add rating property
   },
   {
-    name: 'Customer 3',
-    description: 'review',
-    image: 'assets/default.jpg',
+    name: 'Dyan W.',
+    description: 'John was on time, professional, polite and offered me great prices. It was a pleasure doing business with him, and I will refer him to all my friends. If you have collectibles to sell, call John. You won\'t be sorry!',
+    image: 'assets/reviews/dyan-w.jpg',
     rating: 5 // Add rating property
   },
   {
-    name: 'Customer 4',
-    description: 'review',
-    image: 'assets/default.jpg',
+    name: 'Leon Z.',
+    description: 'I have been buying records from him for few years, I used to buy from other sellers but once I started buying from him I never stopped dealing with him. Nice seller with huge experience in records and other memorabilia item. Pleasure to deal with.',
+    image: 'assets/reviews/leon-z.jpg',
     rating: 5 // Add rating property
   },
   {
-    name: 'Customer 5',
-    description: 'review',
-    image: 'assets/default.jpg',
+    name: 'Randy R.',
+    description: 'John is a pleasure to work with and has been a buyer of many of my collections. Solid business man and very fair. Highly recommended.',
+    image: 'assets/reviews/randy-r.jpg',
     rating: 5 // Add rating property
   },
   {
-    name: 'Customer 6',
-    description: 'review',
-    image: 'assets/default.jpg',
-    rating: 5 // Add rating property
+    name: 'The Critical J.',
+    description: 'This place has vintage to modern electronics ranging from video games, computers, record players, stereos, pops, and cards. The blonde worker and her father are both nice while the father is knowledgeable about the markets and capabilities of all their items.',
+    image: 'assets/reviews/critical-j.jpg',
+    rating: 4 // Add rating property
   }
 
   ];
