869f4f7651af275a5ab202ce2632571b30d6d247
Full Name <theofficialwebsiteguys@gmail.com>
2024-10-24 14:48:52 -0400

    build 2

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+    images: { src: string, alt: string }[] = [
+      { src: 'assets/store1.jpg', alt: 'Store' },
+      { src: 'assets/store2.jpg', alt: 'Store' },
+      { src: 'assets/store3.jpg', alt: 'Store' },
+      { src: 'assets/store4.jpg', alt: 'Store' },
+      { src: 'assets/store5.jpg', alt: 'Store' },
+      { src: 'assets/coins.jpg', alt: 'Store' },
+    ];
 
 }
